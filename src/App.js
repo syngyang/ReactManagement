40000006d24f9bb8fa4ccff62c1f970091f7316c
syngyang <syngyang@gmail.com>
2020-11-10 14:55:49 +0900

    Apply Material-UI componants

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import Customer from './components/Customer'
 import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+import React, { Component } from 'react';
+
+const styles = theme => ({
+  root: {
+      width: '100%',
+      marginTop:theme.spacing.unit * 3,
+      overflowX: "auto"
+  },
+  table: {
+      minWidth: 1080
+  }
+})
 
 const customers = [
   {
   'id': 1,
-  'image': 'https://placeimg.com/64/64/any',
+  'image': 'https://placeimg.com/64/64/1',
   'name': 'Steve Yang',
   'birthday': '911212',
   'gender': '남자',
@@ -12,7 +31,7 @@ const customers = [
 },
 {
   'id': 2,
-  'image': 'https://placeimg.com/64/64/any',
+  'image': 'https://placeimg.com/64/64/2',
   'name': '홍길동',
   'birthday': '921212',
   'gender': '남자',
@@ -20,7 +39,7 @@ const customers = [
 },
 {
   'id': 3,
-  'image': 'https://placeimg.com/64/64/any',
+  'image': 'https://placeimg.com/64/64/3',
   'name': 'Jane Doe',
   'birthday': '9361212',
   'gender': '여자',
@@ -29,54 +48,45 @@ const customers = [
 ]
 
 
-function App() {
-  return (
-    <div>
-       {
-         customers.map((c)=> {
-           return (
-             <Customer 
-              key={c.id}
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-             />
-           )
-         })
-       
-       /* <Customer
-    id={customers[0].id}
-    image= {customers[0].image}
-    name={customers[0].name}
-    birthday= {customers[0].birthday}
-    gender={customers[0].gender}
-    job={customers[0].job}
+class App extends Component {  
+  render() {
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table} >
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              customers.map((c)=> {
+                return (
+                  <Customer 
+                    key={c.id}
+                    id={c.id}
+                    image={c.image}
+                    name={c.name}
+                    birthday={c.birthday}
+                    gender={c.gender}
+                    job={c.job}
+                  />
+                )
+              })
+            }
+          </TableBody>
+        </Table>
   
-  />
-  <Customer
-    id={customers[1].id}
-    image= {customers[1].image}
-    name={customers[0].name}
-    birthday= {customers[1].birthday}
-    gender={customers[1].gender}
-    job={customers[1].job}
-  
-  />
-  <Customer
-    id={customers[2].id}
-    image= {customers[2].image}
-    name={customers[2].name}
-    birthday= {customers[2].birthday}
-    gender={customers[2].gender}
-    job={customers[2].job}
-  
-  /> */}
-    </div>
+      </Paper>
  
-  );
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
